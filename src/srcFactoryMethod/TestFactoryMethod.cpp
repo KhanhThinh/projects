64120e6ed83e7ef0ec201fe729ab4c3419cfbdf4
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-06-04 15:26:01 +0700

    210604 - BLTAnh - Restructure

--- src/srcFactoryMethod/TestFactoryMethod.cpp
+++ src/srcFactoryMethod/TestFactoryMethod.cpp
@@ -1,24 +1,17 @@
 #include "header.h"
-#include "factory.h"
 
 int main(void){
-  //Giả sử bây giờ tôi muốn xử lý đơn nhập hàng của một cửa hàng
-  //Bao gồm một số lượng xe bất kỳ, và ngẫu nhiên nhiều dòng xe khác nhau
-  cout << "Nhap so luong xe can nhap ve cua hang: ";
-  int n; cin >> n;
-
-  //Tạo một vector để quản lý các dòng xe:
   vector<Cars*> arrCars;
-  arrCars.resize(n);
-
-  //Xu ly nhap dong xe
-  cout << "1. Dong xe ban tai - 2. Dong xe the thao - 3. Dong xe mui tran" << endl;
-  for (int i = 0; i < n; i++){
-    cout << "Nhap dong xe thu " << i+1 << ": ";
-    int type; cin >> type;
-    if (type > 0 && type <= 3){
-      arrCars.at(i) = CarsFactory::produceCars(static_cast<eCars>(type));
-    } 
+  int type; int i = 0;
+  cout << "1 - Pickup Truck, 2 - Sports Cars, 3 - Convertible Cars, 0 - Exit" << endl;
+  while (true)
+  {
+    cout << "Your type of car " << i + 1 << ": "; 
+    cin >> type;
+    if (type == 0) break;
+    arrCars.push_back(CarsFactory::produceCars(static_cast<eCars>(type)));
+    i++;
   }
+  output(arrCars);
   return 0;
 }
--- src/srcFactoryMethod/header.h
+++ src/srcFactoryMethod/header.h
@@ -44,74 +44,21 @@ enum eCars{
 };
 
 class CarsFactory{
-protected:
-  int n;
 public:
-  static vector<Cars*> arrCars;
-  void inputCars(){
-    cout << "Input the number of cars imported to your store: ";
-    cin >> n;
-    arrCars.resize(n);
-  }
-  vector<Cars*> getArrCars(){
-    return arrCars;
-  }
-
-  int getNumber(){
-    return n;
-  }
-
   static Cars* produceCars(eCars _carsID){
-    Cars* newCar = NULL;
     switch (_carsID)
     {
-      case PICKUP_TRUCK:
-        newCar = new PickUpTruck();
-        break;
-      case SPORTS:
-        newCar = new SportsCar();
-        break;
-      case CONVERTIBLE:
-        newCar = new ConvertibleCar();
-        break; 
-      default:
-        cout << "Invalid car!!!" << endl;
-        return NULL;
-    }
-    arrCars.push_back(newCar);
-  }
-  void output(){
-    for (int i = 0; i < arrCars.size(); i++){
-      
+      case 1: return new PickUpTruck();
+      case 2: return new SportsCar();
+      case 3: return new ConvertibleCar();
+      default: return NULL;
     }
   }
 };
 
-class OriginalCarsFactory{
-public:
-  virtual Cars* produceCars(eCars _carsID) = 0;
-};
-
-class PickUpFactory: public OriginalCarsFactory{
-public:
-  Cars* produceCars(eCars _carsID){
-    if (_carsID == 1) return new PickUpTruck;
-    else return NULL;
+void output(vector<Cars*>arrCars){
+  for (unsigned int i = 0; i < arrCars.size();i++){
+    arrCars.at(i)->output();
+    arrCars.at(i)->moveToStore();
   }
-};
-
-class SportsFactory: public OriginalCarsFactory{
-public:
-  Cars* produceCars(eCars _carsID){
-    if (_carsID == 2) return new SportsCar;
-    else return NULL;
-  }
-};
-
-class ConvertibleFactory: public OriginalCarsFactory{
-public:
-  Cars* produceCars(eCars _carsID){
-    if (_carsID == 3) return new ConvertibleCar;
-    else return NULL;
-  }
-};
+}
