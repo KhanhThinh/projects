df806be85bdff5b29d164e6c40ade7dda368fb30
Anthony Bùi Lê Tuấn Anh <builetuananh2708@gmail.com>
2021-06-06 14:50:07 +0700

    210606 - BLTAnh - AM Code and images

--- src/srcAbstractFactory/TestAbstractFactory.cpp
+++ src/srcAbstractFactory/TestAbstractFactory.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
-
-using namespace std;
+#include "cararray.h"
+#include "vanarray.h"
 
 int main(void){
+  //Khu vực xe kinh doanh
+  CarsArray* a = new CarsArray();
+  a->input();
+  cout << "_______________" << endl;
+  a->output();
+  cout << "_______________" << endl;
+  a->moveToStore();
+  cout << endl;
+  
+  //Khu vực xe chuyên dụng cho Bộ Y tế
+  VansArray* b = new VansArray();
+  b->input();
+  cout << "________________" << endl;
+  b->outputVan();
+  cout << "________________" << endl;
+  b->deliverToGovernment();
+  cout << endl;
+
   return 0;
 }
